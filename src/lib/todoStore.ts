a5826ae090311890eb2fa846547af5b7fd9bbdab
leifurb <leifurbenedikt@gmail.com>
2023-03-11 22:04:20 +0000

    forgot to change branch:( /added db to todostore

--- src/lib/todoStore.ts
+++ src/lib/todoStore.ts
@@ -1,30 +1,55 @@
 import { uuid } from "uuidv4";
 
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+
+//Assumed i should keep the interface because addTodo depends on it
+// So the db table is wery similar to interface
+// and the addTodo creates the key in the table not the database.
 export interface Todo {
   id: string;
   title: string;
 }
 
-const todos: Todo[] = [];
+//const todos: Todo[] = [];
 
 export async function getTodos() {
-  return todos;
+  return await prisma.newtodo.findMany();
+  //return todos;
 }
 
 export async function addTodo(title: string) {
-  const newTodo = {
+   return await prisma.newtodo.create({
+    data: {
+      id: uuid(),
+      title: title
+    }
+   })
+  /*const newTodo = {
     id: uuid(),
     title: title,
   };
   todos.push(newTodo);
   return newTodo;
+  */
 }
 
 export async function removeTodoById(id: string) {
+  const todo = await prisma.newtodo.delete({
+    where: { id: id },
+  })
+  if (todo){
+    return true
+  }else{
+    return false
+  }
+
+  /*
   const index = todos.findIndex((todo) => todo.id === id);
   if (index !== -1) {
     todos.splice(index, 1);
     return true;
   }
-  return false;
+  return false;*/
 }
